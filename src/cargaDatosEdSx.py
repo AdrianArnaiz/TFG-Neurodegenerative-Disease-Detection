be247823e7c275d4c064dc506e85039e9bc7f98c
aar0062 <aar0062@alu.ubu.es>
2019-05-15 12:16:01 +0200

    Arreglo en clases cargar datos. #24

--- src/cargaDatosEdSx.py
+++ src/cargaDatosEdSx.py
@@ -4,92 +4,92 @@ from sklearn.utils import Bunch
 ruta='./CaracteristicasExtraidas/EdadYSexo/'
 
 def load_art_rt():
-    datos = np.load(self.ruta+'art_rt_ccas.npy')
+    datos = np.load(ruta+'art_rt_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_atleta():
-    datos = np.load(self.ruta+'art_w_atleta_ccas.npy')
+    datos = np.load(ruta+'art_w_atleta_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_braso():
-    datos = np.load(self.ruta+'art_w_braso_ccas.npy')
+    datos = np.load(ruta+'art_w_braso_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_campana():
-    datos = np.load(self.ruta+'art_w_campana_ccas.npy')
+    datos = np.load(ruta+'art_w_campana_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_gato():
-    datos = np.load(self.ruta+'art_w_gato_ccas.npy')
+    datos = np.load(ruta+'art_w_gato_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_petaka():
-    datos = np.load(self.ruta+'art_w_petaka_ccas.npy')
+    datos = np.load(ruta+'art_w_petaka_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_rt():
-    datos = np.load(self.ruta+'fon_rt_ccas.npy')
+    datos = np.load(ruta+'fon_rt_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_v_A():
-    datos = np.load(self.ruta+'fon_v_A_ccas.npy')
+    datos = np.load(ruta+'fon_v_A_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_v_E():
-    datos = np.load(self.ruta+'fon_v_E_ccas.npy')
+    datos = np.load(ruta+'fon_v_E_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_v_I():
-    datos = np.load(self.ruta+'fon_v_I_ccas.npy')
+    datos = np.load(ruta+'fon_v_I_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_v_O():
-    datos = np.load(self.ruta+'fon_v_O_ccas.npy')
+    datos = np.load(ruta+'fon_v_O_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_v_U():
-    datos = np.load(self.ruta+'fon_v_U_ccas.npy')
+    datos = np.load(ruta+'fon_v_U_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_w_atleta():
-    datos = np.load(self.ruta+'fon_w_atleta_ccas.npy')
+    datos = np.load(ruta+'fon_w_atleta_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_w_braso():
-    datos = np.load(self.ruta+'fon_w_braso_ccas.npy')
+    datos = np.load(ruta+'fon_w_braso_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_w_campana():
-    datos = np.load(self.ruta+'fon_w_campana_ccas.npy')
+    datos = np.load(ruta+'fon_w_campana_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_w_gato():
-    datos = np.load(self.ruta+'fon_w_gato_ccas.npy')
+    datos = np.load(ruta+'fon_w_gato_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_fon_w_petaka():
-    datos = np.load(self.ruta+'fon_w_petaka_ccas.npy')
+    datos = np.load(ruta+'fon_w_petaka_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_prs_rt():
-    datos = np.load(self.ruta+'prs_rt_ccas.npy')
+    datos = np.load(ruta+'prs_rt_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
--- src/cargaDatosOrzOff.py
+++ src/cargaDatosOrzOff.py
@@ -4,32 +4,32 @@ from sklearn.utils import Bunch
 ruta='./CaracteristicasExtraidas/Orozco2016/offset/'
 
 def load_art_rt():
-    datos = np.load(self.ruta+'art_rt_ccas.npy')
+    datos = np.load(ruta+'art_rt_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_atleta():
-    datos = np.load(self.ruta+'art_w_atleta_ccas.npy')
+    datos = np.load(ruta+'art_w_atleta_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_braso():
-    datos = np.load(self.ruta+'art_w_braso_ccas.npy')
+    datos = np.load(ruta+'art_w_braso_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_campana():
-    datos = np.load(self.ruta+'art_w_campana_ccas.npy')
+    datos = np.load(ruta+'art_w_campana_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_gato():
-    datos = np.load(self.ruta+'art_w_gato_ccas.npy')
+    datos = np.load(ruta+'art_w_gato_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
 def load_art_w_petaka():
-    datos = np.load(self.ruta+'art_w_petaka_ccas.npy')
+    datos = np.load(ruta+'art_w_petaka_ccas.npy')
     return Bunch(data=datos[:,:-1], target=datos[:,-1])
 
 
